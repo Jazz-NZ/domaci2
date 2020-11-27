0274d3b87f293e16997ff7107466f1d3d9ecdb48
elezboris99 <elez.boris@gmail.com>
2020-11-27 12:43:42 +0100

    Update slike.js

--- Scripts/slike.js
+++ Scripts/slike.js
@@ -2,12 +2,15 @@ $(document).ready(function(){
     let brojac=0;
 $("img").click(function(){
     
-    if(brojac%2==0)
+    if(brojac%3==0)
     {
 $(this).css({"height" : "300px","width" : "400px"});
 }
-else{
+else if(brojac%3==2){
 $(this).css({"height": "150px","width":"200px"});
+}
+else{
+    $(this).css({"height": "450px","width":"600px"});
 }
 brojac++;
 });
