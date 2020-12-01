df2cf89816502df4f545e12c29f8e7e7d8f8fc0a
Lazar-crypto <lazaj1999@gmail.com>
2020-12-01 13:28:42 +0100

    Create kontakt.js

---
+++ Scripts/kontakt.js
@@ -0,0 +1,20 @@
+
+function upozorenje() {
+    
+    
+    var stringX = document.getElementById("kratki_unos_ime").value;
+    var stringY = document.getElementById("kratki_unos_naslov").value;
+    var stringZ = document.getElementById("kratki_unos_email").value;
+    var stringP = document.getElementById("duza_poruka").value;
+    if(stringX==="" || stringY==="" || stringZ=="" || stringP==""){
+        alert("Popunite sva obavezna polja");
+    }
+
+  
+}
+
+function noviProzor(link){
+
+    window.open(link);
+
+}
