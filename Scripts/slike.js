60f76d4c79c95fafb9fdb8d2b2008bcc17b89afe
Lazar-crypto <lazaj1999@gmail.com>
2020-12-01 13:28:52 +0100

    Create slike.js

---
+++ Scripts/slike.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    let brojac=0;
+$("img").click(function(){
+    
+    if(brojac%3==0)
+    {
+$(this).css({"height" : "300px","width" : "400px"});
+}
+else{
+$(this).css({"height": "150px","width":"200px"});
+}
+
+brojac++;
+});
+});
