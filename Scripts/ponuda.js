14cea1794d37ed112d0de78553a097ea64b82c76
Lazar-crypto <lazaj1999@gmail.com>
2020-12-01 13:28:45 +0100

    Create ponuda.js

---
+++ Scripts/ponuda.js
@@ -0,0 +1,50 @@
+function konvertor() {
+    
+    
+    var stringX = document.getElementById("evri").value;
+    var stringY = document.getElementById("kurs").value;
+
+    if(stringX===""||stringY===""){
+        alert("Popunite sva polja")
+    }
+    else{
+
+        //regular expresion proverava da li se nalaze brojevi u promenljivoj
+        var isXnum = /^\d+$/.test(stringX);
+        var isYnum = /^\d+$/.test(stringY);
+        
+         
+
+            
+        
+        if(isXnum==true && isYnum==true){
+
+
+            var x = parseInt(stringX, 10);
+            var y = parseInt(stringY, 10);
+
+            if(x>0 && y>0){
+                var res = x * y;
+    
+                document.getElementById("demo").innerHTML = res;
+
+            }
+            else{
+                alert("Unesi brojevi moraju biti veci od 0")
+            }
+           
+
+        }
+        else{
+            alert("Unesite samo brojeve")
+        }
+    
+        
+    }
+
+    function isNumeric(n) {
+        return !isNaN(parseFloat(n)) && isFinite(n);
+      }
+      
+ 
+    }
