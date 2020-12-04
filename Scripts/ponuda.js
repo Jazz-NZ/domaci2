299c2050d0f2ef88c306c0e2b35ae2fd53c2e117
Jazz-NZ <56455946+Jazz-NZ@users.noreply.github.com>
2020-12-04 12:51:19 +0100

    Update ponuda.js

--- Scripts/ponuda.js
+++ Scripts/ponuda.js
@@ -1,9 +1,12 @@
+
+//konvertor evra u dinare
 function konvertor() {
     
     
     var stringX = document.getElementById("evri").value;
     var stringY = document.getElementById("kurs").value;
-
+    
+    //proverava da li su polja prazana tj. stringovi
     if(stringX===""||stringY===""){
         alert("Popunite sva polja")
     }
@@ -16,16 +19,16 @@ function konvertor() {
          
 
             
-        
+        //provera da li je string broj
         if(isXnum==true && isYnum==true){
 
 
             var x = parseInt(stringX, 10);
             var y = parseInt(stringY, 10);
-
+            //proverava da li su celobrojne vrednosti pozitivne
             if(x>0 && y>0){
                 var res = x * y;
-    
+                //ispisuje konvertovanu vrednost u dinarima
                 document.getElementById("demo").innerHTML = res;
 
             }
@@ -42,6 +45,7 @@ function konvertor() {
         
     }
 
+    //proverava da li je string broj
     function isNumeric(n) {
         return !isNaN(parseFloat(n)) && isFinite(n);
       }
